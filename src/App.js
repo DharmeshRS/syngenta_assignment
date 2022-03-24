2d17ac7e78eb3321cca6ee5211d1e37d28e01f7d
Dharmesh Rajendra Sonar <55545639+DharmeshRS@users.noreply.github.com>
2022-03-24 22:57:30 +0530

    update design

--- src/App.js
+++ src/App.js
@@ -4,7 +4,34 @@ import { useEffect, useState } from 'react';
 function App() {
   let [count, setCount] = useState()
   let [loading, setLoading] = useState(false)
-  let [browserInfo,setBrowserInfo]=useState()
+  let information={
+    "browser_family": "Chrome",
+    "client": {
+    "engine": "Blink",
+    "engine_version": "unknown",
+    "name": "Chrome",
+    "type": "browser",
+    "version": "89.0.4389.114"
+    },
+    "device": {
+    "brand": "Apple",
+    "model": "unknown",
+    "type": "desktop"
+    },
+    "os": {
+    "name": "Mac",
+    "platform": "unknown",
+    "version": "10.15.5"
+    },
+    "os_family": "Mac",
+    "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
+  }
+  let [browserInfo,setBrowserInfo]=useState(information)
+  useEffect( () => {
+  console.log("run only first Time")
+  getData()
+  getBrowserInfo()
+  }, [])
   const getData = () => {
   console.log("api loading...")
   setLoading(true)
@@ -18,36 +45,37 @@ function App() {
   })
 
   }
-  useEffect( () => {
-  console.log("run only first Time")
-  getData()
-  getBrowserInfo()
-  }, [])
 
-  const getBrowserInfo=()=>{
-    console.log("api loading...")
+  
+
+const getBrowserInfo=()=>{
+  console.log("Browser Details API  loading...")
   setLoading(true)
   fetch('https://api.apicagent.com/?ua=Mozilla/5.0%20(Macintosh;%20Intel%20Mac%20OS%20X%2010_15_5)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/89.0.4389.114%20Safari/537.36')
   .then( (response) => {
   response.json().then( (data) => {
   // console.log(data)
-  setBrowserInfo(data)
+    setBrowserInfo(data)
+  
   console.log(data)
   })
   })
   }
+
+
+
   return (
     <div className="App">
     <div className='maindiv'>
        <div className="subdiv">
-            <div><span>Browser Family :</span><span>{browserInfo.browser_family}</span></div>
-            <div><span>Browser Version :</span><span>{browserInfo.client.version}</span></div>
-            <div><span>Browser Device type :</span><span>{browserInfo.device.type}</span></div> 
-            <div><span>Browser OS :</span><span>{browserInfo.os.name}</span></div>
-            <div><span>Browser OS family :</span><span>{browserInfo.os_family}</span></div>
-            <div><span>User Agent :</span><span>{browserInfo.user_agent}</span></div>
+            <div><span>Browser Family :</span><span className='weight'>{browserInfo.browser_family}</span></div>
+            <div><span>Browser Version :</span><span className='weight'>{browserInfo.client.version}</span></div>
+            <div><span>Browser Device type :</span><span className='weight'>{browserInfo.device.type}</span></div> 
+            <div><span>Browser OS :</span><span className='weight'>{browserInfo.os.name}</span></div>
+            <div><span>Browser OS family :</span><span className='weight'>{browserInfo.os_family}</span></div>
+            <div><span>User Agent :</span><span className='weight'>{browserInfo.user_agent}</span></div>
       </div>
-      <div className="subdiv" >
+      <div className="subdiv rightSide" >
               No. of Visitors of Our Website :  <h1>{count}</h1>
         </div>
     </div>
--- src/components/Browser.jsx
+++ src/components/Browser.jsx
@@ -1,19 +1,40 @@
 // import React, { useEffect, useState } from 'react'
 
 // const Browser = () => {
-//   let [info,setInfo]=useState({})
-//   useEffect=(()=>{
-//     setInfo({
-//     browserName: window.navigator.appName,
-//     browserEngine: window.navigator.product,
-//     browserVersion1a: window.navigator.appVersion,
-//     browserVersion1b: navigator.userAgent,
+//   let [browserInfo,setBrowserInfo]=useState()
+//   useEffect(() => {
+//     console.log("run only first Time")
+//     getBrowserInfo()
+//     console.log("browserInfo",browserInfo)
+//     }, [])
+//   const getBrowserInfo=()=>{
+//     console.log("Browser Details API  loading...")
+//     fetch('https://api.apicagent.com/?ua=Mozilla/5.0%20(Macintosh;%20Intel%20Mac%20OS%20X%2010_15_5)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/89.0.4389.114%20Safari/537.36')
+//   .then( (response) => {
+//   response.json()
+//   .then( (data) => {
+//   // console.log(data)
+//   setBrowserInfo(data)
+//   console.log(data)
 //   })
-//   },[])
-  
-// console.log(info)
+//   })
+
+// }
+
+// let {browser_family}=browserInfo;
+
 //   return (
-//     <div>{info}</div>
+//     <div>
+//       <div className="subdiv">
+//       {browser_family}
+//             {/* <div><span>Browser Family :</span><span>{browserInfo.browser_family}</span></div>
+//             <div><span>Browser Version :</span><span>{browserInfo.client.version}</span></div>
+//             <div><span>Browser Device type :</span><span>{browserInfo.device.type}</span></div> 
+//             <div><span>Browser OS :</span><span>{browserInfo.os.name}</span></div>
+//             <div><span>Browser OS family :</span><span>{browserInfo.os_family}</span></div>
+//             <div><span>User Agent :</span><span>{browserInfo.user_agent}</span></div> */}
+//       </div>
+//     </div>
 //   )
 // }
 
