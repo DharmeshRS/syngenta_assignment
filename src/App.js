088a12367f98031b0735bfed6a0157864b3282ca
Dharmesh Rajendra Sonar <55545639+DharmeshRS@users.noreply.github.com>
2022-03-24 19:22:28 +0530

    update counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  let [count, setCount] = useState(1)
+  let [loading, setLoading] = useState(false)
+  // let [dummyState, updateDummyState] = useState(false)
+  // const toggleDummyState = () => {
+  // console.log("toggle dummy state")
+  // dummyState = updateDummyState(!dummyState)
+  // }
+  // useEffect( () => {
+  // console.log("only for this dummy state")
+  // }, [dummyState])
+  // const updateCount = () => {
+  // // setCount(count + 1)
+  // getData()
+  // }
+  const getData = () => {
+  console.log("api loading...")
+  setLoading(true)
+  fetch('https://api.countapi.xyz/hit/dharmesh/visits')
+  .then( (response) => {
+  response.json().then( (data) => {
+  // console.log(data)
+  setCount(data.value)
+  setLoading(false)
+  })
+  })
+  .then( (data) => {
+  setLoading(false)
+  // console.error(data)
+  });
+  }
+  useEffect( () => {
+  console.log("run only first Time")
+  getData()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+              No. of Visitors of Our Website :  <h1>{count}</h1>
+        </div>
     </div>
   );
 }
